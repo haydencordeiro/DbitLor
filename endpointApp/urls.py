bb8a6b1946ed26a1379b856022fa56aae1fbe33f
Hayden Corderio <cordeirohayden@gmail.com>
2021-02-23 11:48:06 +0530

    first commit

---
+++ endpointApp/urls.py
@@ -0,0 +1,15 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from endpointApp import views
+from rest_framework import routers
+from . import views
+
+urlpatterns = [
+    path('', include('djoser.urls')),
+    path('', include('djoser.urls.authtoken'))
+
+
+
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
